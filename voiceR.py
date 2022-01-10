df6bf8c340e0bb3060a597e027685b9ebfaaa27f
Maansi Bisht <Maansibisht466@gmail.com>
2022-01-10 23:49:52 +0530

    Update voiceR.py

--- voiceR.py
+++ voiceR.py
@@ -1,15 +1,56 @@
-import p
+import pyttsx3                        
+import speech_recognition as sr         
+import datetime
+import wikipedia
+import webbrowser
+import os
+import random
+
+engine = pyttsx3.init('sapi5')
+voices = engine.getProperty('voices') 
+
+# print(voices[1].id)
+engine.setProperty('voice', voices[1].id)
+
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+
+def wishMe():
+    hour = int(datetime.datetime.now().hour)
+    if (hour >= 0 and hour < 12):
+        speak('Good Morning!')
+    elif (hour >= 12 and hour < 18):
+        speak('Good Afternoon!')
+    else:
+        speak('Good Evening!')
+
+    speak('I am Jarvis Sir, I am working for Waqar bhai. Please tell me how may I help you')
 
 
-def
-r.pause_threshhold = 1
-r.energy_threshhold = 300
-r.adjust_for_ambient_noise(source, duration=1)
-try:
-	print('Recognizing..')
-	query = r.recognize_google(audio, language='en-us')
-	print(f'
-	
+def takeCommand():
+    # It takes microphone input from the user and returns string output, microphone input requires PyAudio
+    # pip install pyaudio
+
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print('Listening...')
+        r.pause_threshold = 1  # Means it can have a 1 second gap while speaking
+        r.energy_threshold = 300  # minimum audio energy to consider for recording
+        r.adjust_for_ambient_noise(source, duration = 1)
+        audio = r.listen(source)                               #instance of AudioData
+
+        try:
+            print('Recognizing...')
+            query = r.recognize_google(audio, language='en-US')
+            print(f'User Said: {query}\n')
+        except Exception as e:
+            print(e)
+            print('Say that again please...')
+            query = takeCommand().lower()
+            return query;
+        return query;
+
 	
 if __name__ =='__main__':
 	wishMe()
